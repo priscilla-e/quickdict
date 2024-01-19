b9d0750b44ade5fd18664141b1c377b4d8425ae0
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-19 17:11:33 +0000

    Add fonts and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Open Sans', 'sans-serif']
+      },
+      colors: {
+        'primary': '#5E7BE0',
+        'primary-light': '#7089E3',
+        'secondary': '#C4E9F6'
+      }
+    },
   },
   plugins: [],
 }
