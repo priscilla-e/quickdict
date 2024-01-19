795ec1de87efaca40b326ff23569ddfc980e70ac
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-19 19:32:31 +0000

    Sort tailwind classes in header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,10 @@
-import { HiMenuAlt2 } from "react-icons/hi";
+import { HiMenuAlt2 } from 'react-icons/hi';
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between text-white p-4 md:px-14">
-      <div className="text-2xl font-bold hover:cursor">QuickDict</div>
-      <button className="bg-primary-light p-2 rounded-lg text-2xl">
+    <header className="flex items-center justify-between p-4 text-white md:px-14 ">
+      <div className="text-2xl font-bold">QuickDict</div>
+      <button className="rounded-lg bg-primary-light p-2 text-2xl transition-transform duration-300 hover:scale-110 focus:outline-none">
         <HiMenuAlt2 />
       </button>
     </header>
