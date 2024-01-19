82316598bd581b0118139c1efab51ad22c9a98b5
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-19 22:09:15 +0000

    Styling fixes

--- src/components/definition/DefinitionItem.tsx
+++ src/components/definition/DefinitionItem.tsx
@@ -10,15 +10,18 @@ export default function DefinitionItem({ definition }: DefinitionItemProps) {
     <li className="space-y-2">
       <div className="flex items-center justify-start gap-2">
         <span className="font-bold">{definition.partOfSpeech}</span>
-        <span className="text-sm">{`[${definition.ipa}]`}</span>
-        <button>
+        <span className="text-sm italic">{`/${definition.ipa}/`}</span>
+        <button className='text-xl focus:outline-none' onClick={() => alert('TODO: playAudio')}>
           <HiSpeakerWave />
         </button>
       </div>
       <div className="ml-4 md:ml-8">
-        <ul className="text-sm">
+        <ul className="text-sm space-y-4">
           {definition.meanings.map((meaning, index) => (
-            <li key={index}>{`${index + 1}. ${meaning}`}</li>
+            <li key={index} className="flex gap-3 max-w-lg">
+              <span>{`${index + 1}.`}</span>
+              <span>{meaning}</span>
+            </li>
           ))}
         </ul>
       </div>
