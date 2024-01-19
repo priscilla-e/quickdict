a0735da7971b3c35fdfc018cbf088b0eb30c1d46
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-19 20:19:01 +0000

    Refactor searchout component

--- src/components/meaning/Meaning.tsx
+++ src/components/meaning/Meaning.tsx
@@ -1,6 +1,6 @@
 import { HiSpeakerWave } from 'react-icons/hi2';
 
-export default function SearchOutput() {
+export default function Meaning() {
   return (
     <div className="mx-auto mt-10 min-h-96 max-w-2xl rounded-3xl bg-white px-6 py-8 md:p-10">
       <div className="flex items-center justify-between rounded-2xl bg-primary p-6 text-white md:py-10">
