e7bd332a1d19f40c02fe2588c95ceffb4ac2da8c
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-20 20:14:58 +0000

    Add docstring to fetchfromfirebase util function

--- src/utils/audioParser.ts
+++ src/utils/audioParser.ts
@@ -3,11 +3,11 @@ import { AudioFormat } from "../types";
 /**
  * Audio Parser for Merriam-Webster's API, following their audio path rules.
  * 
- * Reference: https://dictionaryapi.com/products/json#sec-2.prs
+ * @see {@link https://dictionaryapi.com/products/json#sec-2.prs}
  * 
  * @param baseFileName The base name of the audio file name without the extension.
  * @param format The required audio format (e.g 'mp3', 'wav' or 'ogg').  Defaults to mp3.
- * @returns {string} The constructed URL to the audio file in Merriam-Webster's Media Servers.
+ * @returns The constructed URL to the audio file in Merriam-Webster's Media Servers.
  */
 export default function audioParser(baseFileName: string, format: AudioFormat = 'mp3') {
     let subDirectory = 'audio'
--- src/utils/fetchFromFirebase.ts
+++ src/utils/fetchFromFirebase.ts
@@ -1,20 +1,27 @@
-import {ref, get, child} from "firebase/database";
-import {database} from '../firebaseConfig.ts'
-import { Definition, SearchResult } from "../types.ts";
+import { ref, get, child } from 'firebase/database';
+import { database } from '../firebaseConfig.ts';
+import { SearchResult } from '../types.ts';
 
-
+/**
+ * Fetches cached search result from Firebase.
+ *
+ * @param word The word to find result for.
+ * @returns The result result if found or null otherwise. 
+ */
 export default async function fetchFromFirebase(word: string) {
   const dbRef = ref(database);
   try {
-    const snapshot = await get(child(dbRef, `cache/${word}`))
+    const snapshot = await get(child(dbRef, `cache/${word}`));
 
     if (snapshot.exists()) {
-      const definitions: Definition[] = snapshot.val();
-      return {word, definitions} as SearchResult;
+      const searchResult: SearchResult = {
+        word,
+        definitions: snapshot.val(),
+      };
+      return searchResult;
     }
-  }
-  catch (error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
   return null;
 }
