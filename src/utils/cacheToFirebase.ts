34f86eb4c67a297fe768b88173494e88ba3cdcd7
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-20 19:57:41 +0000

    Add cache to firebase utility function

---
+++ src/utils/cacheToFirebase.ts
@@ -0,0 +1,13 @@
+import { database } from '../firebaseConfig.ts';
+import { ref, set } from 'firebase/database';
+import type { SearchResult } from '../types.ts';
+
+/**
+ * Writes a search result to Firebase Realtime Database.
+ *
+ * @param searchResult The search result.
+ */
+export default function cacheToFirebase(searchResult: SearchResult) {
+  const cacheRef = ref(database, `cache/${searchResult.word}`);
+  set(cacheRef, searchResult.definitions);
+}
