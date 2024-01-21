6c81af1e18bddf0e72a3e29111764ea5b2a96154
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-21 11:28:03 +0000

    Refactor Definition item component

--- src/components/definition/DefinitionItem.tsx
+++ src/components/definition/DefinitionItem.tsx
@@ -7,20 +7,24 @@ interface DefinitionItemProps {
 }
 
 export default function DefinitionItem({ definition }: DefinitionItemProps) {
+  const audio = definition.pronunciations.audio;
+  const mw = definition.pronunciations.mw;
+
   return (
     <li className="space-y-2 border-b border-b-gray-200 py-6 ">
       <div className="flex items-center justify-start gap-2">
         <span className="font-bold">{definition.functionalLabel}</span>
-        <span className="text-sm italic">{`/${definition.pronunciations.mw}/`}</span>
-        {definition.pronunciations.audio && (
+        {mw && <span className="text-sm italic">{`/${mw}/`}</span>}
+        {audio && (
           <button
             className="text-xl focus:outline-none"
-            onClick={() => playAudio(definition.pronunciations.audio)}
+            onClick={() => playAudio(audio)}
           >
             <HiSpeakerWave />
           </button>
         )}
       </div>
+
       <div className="ml-4 md:ml-8">
         <ul className="space-y-4 text-sm">
           {definition.meanings.map((meaning, index) => (
