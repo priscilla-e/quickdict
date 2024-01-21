4aa3e5cf43a2a38c8168c817bfd4df213b7a75ef
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-21 11:51:02 +0000

    Refactor search results

--- src/components/search/SearchResult.tsx
+++ src/components/search/SearchResult.tsx
@@ -9,27 +9,30 @@ interface SearchResultProps {
 
 export default function SearchResult({ searchResult }: SearchResultProps) {
   const [firstResult] = searchResult.definitions;
+  const audio = firstResult.pronunciations.audio;
+  const mw = firstResult.pronunciations.mw;
+  
   return (
     <div className="mx-auto mt-10 min-h-96 max-w-2xl rounded-3xl bg-white px-6 py-8 md:p-10">
+      {/* Result Header Card */}
       <div className="flex items-center justify-between rounded-2xl bg-primary p-6 text-white md:py-10">
         <div>
           <span className="block text-xl font-bold md:text-3xl">
             {searchResult.word}
           </span>
-          {firstResult.pronunciations.mw && (
-            <span className="text-xs">{`[ ${firstResult.pronunciations.mw} ]`}</span>
-          )}
+          {mw && <span className="text-xs">{`[ ${mw} ]`}</span>}
         </div>
-        {firstResult.pronunciations.audio && (
+        {audio && (
           <button
             className="text-3xl duration-300 hover:scale-105 hover:text-gray-100 focus:outline-none"
-            onClick={() => playAudio(firstResult.pronunciations.audio)}
+            onClick={() => playAudio(audio)}
           >
             <HiSpeakerWave />
           </button>
         )}
       </div>
 
+      {/* Result Definitions */}
       <DefinitionList definitions={searchResult.definitions} />
     </div>
   );
