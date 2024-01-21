d0c33b267cd8933a6a842b7ee6241c76b17d0dba
Priscilla Emasoga <priscillaemasoga@gmail.com>
2024-01-21 11:50:53 +0000

    Add styling to no results component

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import { useEffect, useState } from 'react';
 import Header from './components/layout/Header.tsx';
 import SearchInput from './components/search/SearchInput.tsx';
 import SearchResult from './components/search/SearchResult.tsx';
+import NoResult from './components/search/NoResult.tsx';
 import type { SearchResult as SearchResultType } from './types.ts';
-import { cacheToFirebase, fetchFromFirebase, fetchFromAPI, getRandomWord} from './utils';
-
+import {
+  cacheToFirebase,
+  fetchFromFirebase,
+  fetchFromAPI,
+  getRandomWord,
+} from './utils';
 
 function App() {
-  const [searchResult, setSearchResult] = useState<SearchResultType | null>(null);
-  
-  // TODO: Add random first word feature
-  useEffect(() => {
-    handleSearh(getRandomWord());
-  },[])
+  const [searchResult, setSearchResult] = useState<SearchResultType | null>(
+    null
+  );
+
+  // useEffect(() => {
+  //   // TODO: Display user's last search else random word
+  //   handleSearh(getRandomWord());
+  // }, []);
 
   const handleSearh = async (searchTerm: string) => {
     // Fetch word from local storage
@@ -27,7 +34,7 @@ function App() {
     const firebaseRes = await fetchFromFirebase(searchTerm);
     if (firebaseRes) {
       console.log('Firebase!', firebaseRes);
-      setSearchResult(firebaseRes)
+      setSearchResult(firebaseRes);
 
       // Cache this res to the user's storage so we retrieve from there next time
       localStorage.setItem(searchTerm, JSON.stringify(firebaseRes));
@@ -35,22 +42,21 @@ function App() {
     }
 
     // If not found, fetch from API
-    const apiRes = await fetchFromAPI(searchTerm)
+    const apiRes = await fetchFromAPI(searchTerm);
     if (apiRes) {
       console.log('API!', apiRes);
-      setSearchResult(apiRes)
+      setSearchResult(apiRes);
 
       // Cache to firebase to and user's localStorage
-      cacheToFirebase(apiRes)
+      cacheToFirebase(apiRes);
       localStorage.setItem(searchTerm, JSON.stringify(apiRes));
-      return 
+      return;
     }
 
-    // No Result Found! 
-    setSearchResult(null)
+    // No Result Found!
+    setSearchResult(null);
     alert(searchTerm);
   };
-
 
   return (
     <>
@@ -60,7 +66,11 @@ function App() {
           <SearchInput onSearch={handleSearh} />
         </div>
 
-        {searchResult && <SearchResult searchResult={searchResult} />}
+        {searchResult ? (
+          <SearchResult searchResult={searchResult} />
+        ) : (
+          <NoResult />
+        )}
       </main>
     </>
   );
--- src/components/search/NoResult.tsx
+++ src/components/search/NoResult.tsx
@@ -1,3 +1,16 @@
+import { TbMoodEmpty } from 'react-icons/tb';
+
 export default function NoResult() {
-  return <div>No result</div>;
+  return (
+    <div className="mx-auto mt-10 flex min-h-96 max-w-2xl flex-col items-center justify-center gap-2 rounded-3xl bg-white px-6 py-8 text-center text-gray-400 md:p-10">
+      <TbMoodEmpty className="mx-auto animate-pulse text-8xl" />
+      <p className="mt-2 text-xl font-bold md:text-2xl">
+        {'Sorry! No results found :('}
+      </p>
+      <p className="max-w-md">
+        We couldn't find the word you are loking for. Please check the spelling
+        or try a different word!
+      </p>
+    </div>
+  );
 }
